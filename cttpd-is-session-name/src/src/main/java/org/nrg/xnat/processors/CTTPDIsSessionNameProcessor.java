24891e9bf74cf246cd75a2748961ecdc54ab9826
Tim Rosenow <Tim.Rosenow@uwa.edu.au>
2024-09-18 13:32:13 +0800

    fixed errors and XNAT version update

--- cttpd-is-session-name/src/src/main/java/org/nrg/xnat/processors/CTTPDIsSessionNameProcessor.java
+++ cttpd-is-session-name/src/src/main/java/org/nrg/xnat/processors/CTTPDIsSessionNameProcessor.java
@@ -20,40 +20,41 @@ import java.util.Map;
 
 @Component
 @Slf4j
-public class CTTPDIsSessionNameProcessor extends AbstractArchiveProcessor {
+public class StudyRemappingArchiveProcessor extends AbstractArchiveProcessor {
 
-// edits up to here
     @Override
     public boolean process(final DicomObject dicomData, final SessionData sessionData, final MizerService mizer, ArchiveProcessorInstance instance, Map<String, Object> aeParameters) throws ServerException{
         try {
             final String studyInstanceUID = dicomData.getString(Tag.StudyInstanceUID);
 
+
             // Default Study script, replaced with this hard-coded script
-	    // String script = DefaultAnonUtils.getService().getStudyScript(studyInstanceUID);
-	    String script = "version \"6.4\"\n";
-	    // Create a new session ID, because XNAT needs session to be unique.
-	    // XNAT obtains it from Study Description (or StudyID prior to Paravision 360.3.4)
-	    // We save this session ID to (0012,0051) - Clinical Trial Time
-	    // Point Description (CTTPD) - and use this as the import filter.
-	    //
-	    // If StudyDesc exists, set CTTPD to PatientID:StudyDesc
-	    // If not, set it to PatientID:StudyID
-	    // Finally all else fails, PatientID:StudyDate
-	    // StudyDesc is (0008,1030)
-	    // StudyID is (0020,0010)
-	    script = script + "PatientID := (0010,0020)\n";
-	    script = script + "StudyDate := (0008,0020)\n";
+            // String script = DefaultAnonUtils.getService().getStudyScript(studyInstanceUID);
+            String script = "version \"6.4\"\n";
+            // Create a new session ID, because XNAT needs session to be unique.
+            // XNAT obtains it from Study Description (or StudyID prior to Paravision 360.3.4)
+            // We save this session ID to (0012,0051) - Clinical Trial Time
+            // Point Description (CTTPD) - and use this as the import filter.
+            //
+            // If StudyDesc exists, set CTTPD to PatientID:StudyDesc
+            // If not, set it to PatientID:StudyID
+            // Finally all else fails, PatientID:StudyDate
+            // StudyDesc is (0008,1030)
+            // StudyID is (0020,0010)
+            script = script + "PatientID := (0010,0020)\n";
+            script = script + "StudyDate := (0008,0020)\n";
 
-	    // If StudyDesc doesn't exist, use StudyID instead
-	    script = script + "(0008,1030) !~ \"[a-zA-Z_0-9]*\" ? StudyDesc := (0020,0010) : StudyDesc := (0008,1030)\n";
+            // If StudyDesc doesn't exist, use StudyID instead
+            script = script + "(0008,1030) !~ \"[a-zA-Z_0-9]*\" ? StudyDesc := (0020,0010) : StudyDesc := (0008,1030)\n";
 
-	    // Fix Adam's staff's dumb typos: "Agrenica"
-	    script = script + "PatientID := replace[ PatientID, \"Agrenica\", \"Argenica\" ]\n";
-	    script = script + "StudyDesc := replace[ StudyDesc, \"Agrenica\", \"Argenica\" ]\n";
-	    // If StudyDesc does not contain PatientID, then we include StudyID at the start.
-	    script = script + "TestString := format[ \".*{0}.*\", PatientID ]\n";
-	    script = script + "StudyDesc !~ TestString ? StudyDesc := format[ \"{0}_{1}\", PatientID, StudyDesc ] : StudyDesc := StudyDesc\n";
-	    script = script + "(0012,0051) := StudyDesc\n";
+            // Fix Adam's staff's dumb typos: "Agrenica"
+            script = script + "PatientID := replace[ PatientID, \"Agrenica\", \"Argenica\" ]\n";
+            script = script + "StudyDesc := replace[ StudyDesc, \"Agrenica\", \"Argenica\" ]\n";
+            // If StudyDesc does not contain PatientID, then we include StudyID at the start.
+            script = script + "TestString := format[ \".*{0}.*\", PatientID ]\n";
+            script = script + "StudyDesc !~ TestString ? StudyDesc := format[ \"{0}_{1}\", PatientID, StudyDesc ] : StudyDesc := StudyDesc\n";
+            script = script + "(0012,0051) := StudyDesc\n";
+
 
             if(StringUtils.isNotBlank(script)){
                 String proj = "";
@@ -77,6 +78,8 @@ public class CTTPDIsSessionNameProcessor extends AbstractArchiveProcessor {
         } catch (Throwable e) {
             log.debug("Dicom anonymization failed: " + dicomData, e);
             throw new ServerException(Status.SERVER_ERROR_INTERNAL,e);
+        }
+        return true;
     }
 }
 
