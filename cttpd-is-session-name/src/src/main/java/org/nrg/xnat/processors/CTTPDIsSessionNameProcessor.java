e1230222fb2cd15763360df4350b1016e0f4c210
Tim Rosenow <Tim.Rosenow@uwa.edu.au>
2024-09-18 13:14:57 +0800

    Deps

--- cttpd-is-session-name/src/src/main/java/org/nrg/xnat/processors/CTTPDIsSessionNameProcessor.java
+++ cttpd-is-session-name/src/src/main/java/org/nrg/xnat/processors/CTTPDIsSessionNameProcessor.java
@@ -5,10 +5,10 @@ import org.apache.commons.lang.StringUtils;
 import org.dcm4che2.data.DicomObject;
 import org.dcm4che2.data.Tag;
 import org.nrg.action.ServerException;
-//import org.nrg.dicom.mizer.objects.AnonymizationResult;
-//import org.nrg.dicom.mizer.objects.AnonymizationResultError;
-//import org.nrg.dicom.mizer.objects.AnonymizationResultNoOp;
-//import org.nrg.dicom.mizer.objects.AnonymizationResultReject;
+import org.nrg.dicom.mizer.objects.AnonymizationResult;
+import org.nrg.dicom.mizer.objects.AnonymizationResultError;
+import org.nrg.dicom.mizer.objects.AnonymizationResultNoOp;
+import org.nrg.dicom.mizer.objects.AnonymizationResultReject;
 import org.nrg.dicom.mizer.service.MizerService;
 import org.nrg.xnat.entities.ArchiveProcessorInstance;
 import org.nrg.xnat.helpers.merge.anonymize.DefaultAnonUtils;
@@ -64,12 +64,42 @@ public class CTTPDIsSessionNameProcessor extends AbstractArchiveProcessor {
                     subj = sessionData.getSubject();
                     folder = sessionData.getFolderName();
                 }
-                mizer.anonymize(dicomData, proj, subj, folder, script, true);
+                AnonymizationResult result = mizer.anonymize(dicomData, proj, subj, folder, script, true);
+                if (result instanceof AnonymizationResultError) {
+                    String msg = result.getMessage();
+                    log.debug("Dicom anonymization failed: {}: {}", dicomData, msg);
+                    throw new ServerException(Status.SERVER_ERROR_INTERNAL,msg);
+                }
+                if ( result instanceof AnonymizationResultNoOp) {
+                    return false;
+                }
+            }
+        } catch (Throwable e) {
+            log.debug("Dicom anonymization failed: " + dicomData, e);
+            throw new ServerException(Status.SERVER_ERROR_INTERNAL,e);
+    }
+}
+
+	if(StringUtils.isNotBlank(script)){
+                String proj = "";
+                String subj = "";
+                String folder = "";
+                if(sessionData!=null){
+                    proj = sessionData.getProject();
+                    subj = sessionData.getSubject();
+                    folder = sessionData.getFolderName();
+                }
+                AnonymizationResult result = mizer.anonymize(dicomData, proj, subj, folder, script, true);
+                if (result instanceof AnonymizationResultError) {
+                    String msg = result.getMessage();
+                    log.debug("Dicom anonymization failed: {}: {}", dicomData, msg);
+                    throw new ServerException(Status.SERVER_ERROR_INTERNAL,msg);
+                }
+                if ( result instanceof AnonymizationResultNoOp) {
+                    return false;
+                }
             }
         } catch (Throwable e) {
             log.debug("Dicom anonymization failed: " + dicomData, e);
             throw new ServerException(Status.SERVER_ERROR_INTERNAL,e);
         }
-        return true;
-    }
-}
