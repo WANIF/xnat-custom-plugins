e5d527b45c2e2b5bb6a9dd81baa273c81a8f4b7f
Tim Rosenow <Tim.Rosenow@uwa.edu.au>
2024-09-18 11:54:31 +0800

    oops misnamed the file

--- cttpd-is-session-name/src/src/main/java/org/nrg/xnat/plugins/MRSessionInfoBValuePlugin.java
+++
@@ -1,8 +0,0 @@
-package org.nrg.xnat.plugins;
-
-import org.nrg.framework.annotations.XnatPlugin;
-
-@XnatPlugin(value = "CTTPDIsSessionName", name = "CTTPD Is Session Name plugin")
-public class CTTPDIsSessionNamePlugin {
-}
-
