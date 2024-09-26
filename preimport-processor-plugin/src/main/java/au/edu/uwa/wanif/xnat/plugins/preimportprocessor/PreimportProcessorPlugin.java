a37f4cbbbc444fb13910be8c6b74849320e9e5a6
Tim Rosenow <Tim.Rosenow@uwa.edu.au>
2024-09-26 13:40:55 +0800

    alpha.1

---
+++ preimport-processor-plugin/src/main/java/au/edu/uwa/wanif/xnat/plugins/preimportprocessor/PreimportProcessorPlugin.java
@@ -0,0 +1,29 @@
+/*
+ * Preimport Processor Plugin
+ * XNAT https://www.xnat.org
+ *
+ * Tim Rosenow
+ * The University of Western Australia
+ */
+
+package au.edu.uwa.wanif.xnat.plugins.preimportprocessor;
+
+//import lombok.extern.slf4j.Slf4j;
+//import org.dcm4che2.data.Tag;
+//import org.nrg.config.services.ConfigService;
+//import org.nrg.framework.annotations.XnatDataModel;
+import org.nrg.framework.annotations.XnatPlugin;
+//import org.nrg.xdat.bean.TemplateSampleBean;
+//import org.nrg.xdat.security.user.XnatUserProvider;
+//import org.nrg.xnat.plugins.template.dcm.ConfigurableMappedAttributeExtractor;
+//import org.nrg.xnat.plugins.template.dcm.ConfigurableMappedDicomObjectIdentifier;
+//import org.nrg.xnat.plugins.template.dcm.ConfigurableMappedNumberExtractor;
+//import org.nrg.xnat.plugins.template.dcm.ConfigurableMappedProjectIdentifier;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+
+@XnatPlugin(value = "PreimportProcessorPlugin", name = "Preimport Processor Plugin")
+@ComponentScan({"oau.edu.uwa.wanif.xnat.plugins.preimportprocessor.preferences"})
+//@Slf4j
+public class PreimportProcessorPlugin {
+}
